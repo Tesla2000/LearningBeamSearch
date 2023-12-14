25d675b0bb8ede990c1e78f86b80ef3d1b156f51
Tesla2000 <fratajczak124@gmail.com>
2023-12-14 17:43:12 +0100

    updated display

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from itertools import count
 from math import sqrt
 from statistics import mean
 
+import numpy as np
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
@@ -12,7 +13,7 @@ from ml_models.DataMaker import DataMaker
 
 
 def train(model: nn.Module, n_tasks: int, m_machines: int, rows: int):
-    patience = 200
+    patience = 1000
     average_size = 100
     batch_size = 16
     learning_rate = 0.0001
@@ -22,7 +23,7 @@ def train(model: nn.Module, n_tasks: int, m_machines: int, rows: int):
     data_maker = DataMaker(n_tasks=n_tasks, m_machines=m_machines, rows=rows, length=batch_size)
     train_loader = DataLoader(data_maker, batch_size=batch_size, shuffle=True)
     losses = deque(maxlen=average_size)
-    best_loss = 1e10
+    best_loss = float('inf')
     best_index = 0
     for epoch in count():
         running_loss = 0
@@ -34,19 +35,26 @@ def train(model: nn.Module, n_tasks: int, m_machines: int, rows: int):
             loss = criterion(outputs, target)
             loss.backward()
             optimizer.step()
-            running_loss += loss.item()
+            running_loss += sqrt(loss.item())
         losses.append(running_loss)
-        average = mean(losses) / batch_size
-        print(epoch, sqrt(average))
+        average = mean(loss / batch_size for loss in losses)
+        print(epoch, average)
         if average < best_loss:
             best_loss = average
             best_index = epoch
+            model_weights = model.state_dict()
         if best_index + patience < epoch:
-            print(best_loss)
+            print(sqrt(best_loss))
+            torch.save(
+                model_weights,
+                f'output_models/{rows}_{best_loss:.3f}.pth',
+            )
             break
 
 
 if __name__ == '__main__':
+    torch.manual_seed(42)
+    np.random.seed(42)
     n_tasks = 7
     m_machines = 10
     rows = 5
