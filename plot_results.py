9d2a97b1e8166673db7ca5dcb400021d3d9991e8
Tesla2000 <fratajczak124@gmail.com>
2024-02-10 19:08:13 +0100

    Updated plotting

--- plot_results.py
+++ plot_results.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     labels = []
     for log_file in Config.MODEL_RESULTS.iterdir():
         data = np.loadtxt(log_file, delimiter=',')
-        x = np.linspace(*data[[0, -1], 0], num=len(data))[:
-                                                          # Config.minimal_counting_epoch_number
+        x = np.linspace(*data[[0, -1], 0], num=len(data))[
+                                                          Config.minimal_counting_epoch_number:
             ]
-        y = data[:
-                 # Config.minimal_counting_epoch_number
+        y = data[
+                 Config.minimal_counting_epoch_number:
         , 1]
         del data
         plt.plot(x, y)
